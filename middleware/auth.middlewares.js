a64ea6dc82b69aa750216ed4577e3cddf46b36ec
Anish-Jha <anishj545@gmail.com>
2023-05-13 01:01:14 +0530

    file

--- middleware/auth.middlewares.js
+++ middleware/auth.middlewares.js
@@ -1,22 +1,22 @@
-const jwt=require('jsonwebtoken')
-require("dotenv").config();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const auth=(req,res,next)=>{
-    const token=req.headers.authorization.split(" ")[1];
+const auth = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  try {
     if (token) {
-      const decoded=jwt.verify(token, process.env.key);
-      if (decoded){
-        req.body.userID = decoded.userID;
-        req._id=decoded.userID;
-        next();
-      } else {
-        res.status(400).send({ message: "Please Login first" });
-      }
+      const decoded = jwt.verify(token, process.env.key);
+      req.body.userID = decoded.userID;
+      req._id = decoded.userID;
+      next();
     } else {
-      res.status(400).send({ message: "Please Login first" });
+      throw new Error('Authorization token not found');
     }
-}
+  } catch (error) {
+    console.error(error);
+    res.status(401).send({ message: 'Unauthorized access' });
+  }
+};
 
-module.exports=auth
-
-
+module.exports = auth;
