f5df4450993beae7fd15438dd519a5c6daa8d6f4
Anish-Jha <anishj545@gmail.com>
2023-05-13 01:52:54 +0530

    file

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt=require('jsonwebtoken')
+require("dotenv").config();
+
+const auth=(req,res,next)=>{
+    const token=req.headers.authorization.split(" ")[1];
+    if (token) {
+      const decoded=jwt.verify(token, process.env.key);
+      if (decoded){
+        req.body.userID = decoded.userID;
+        req._id=decoded.userID;
+        next();
+      } else {
+        res.status(400).send({ message: "Please Login first" });
+      }
+    } else {
+      res.status(400).send({ message: "Please Login first" });
+    }
+}
+
+module.exports=auth
+
+
