232e4f20e5b6cceb63265e03756efd4fa89834ee
Anish-Jha <anishj545@gmail.com>
2023-05-13 01:06:46 +0530

    file

--- middleware/auth.middlewares.js
+++ middleware/auth.middlewares.js
@@ -2,14 +2,17 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const auth = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
-
   try {
+    const token = req.headers.authorization?.split(' ')[1];
     if (token) {
       const decoded = jwt.verify(token, process.env.key);
-      req.body.userID = decoded.userID;
-      req._id = decoded.userID;
-      next();
+      if (decoded.userID) {
+        req.body.userID = decoded.userID;
+        req._id = decoded.userID;
+        next();
+      } else {
+        throw new Error('Invalid token');
+      }
     } else {
       throw new Error('Authorization token not found');
     }
