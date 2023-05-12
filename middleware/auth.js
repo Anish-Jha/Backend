dc7e8d62c5f2add3b5ab4528a06699e1cb1764f8
Anish-Jha <anishj545@gmail.com>
2023-05-13 02:08:34 +0530

    file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,22 @@
-const jwt=require('jsonwebtoken')
-require("dotenv").config();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const auth=(req,res,next)=>{
-    const token=req.headers.authorization.split(" ")[1];
-    if (token) {
-      const decoded=jwt.verify(token, `${process.env.SECRET_KEY}`);
-      if (decoded){
-        req.body.userID = decoded.userID;
-        req._id=decoded.userID;
-        next();
-      } else {
-        res.status(400).send({ message: "Please Login first" });
-      }
+const auth = (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1];
+  if (token) {
+    const secretKey = process.env.SECRET_KEY;
+    console.log('Secret Key:', secretKey); 
+    const decoded = jwt.verify(token, secretKey);
+    if (decoded) {
+      req.body.userID = decoded.userID;
+      req._id = decoded.userID;
+      next();
     } else {
-      res.status(400).send({ message: "Please Login first" });
+      res.status(400).send({ message: 'Please Login first' });
     }
-}
+  } else {
+    res.status(400).send({ message: 'Please Login first' });
+  }
+};
 
-module.exports=auth
-
-
+module.exports = auth;
