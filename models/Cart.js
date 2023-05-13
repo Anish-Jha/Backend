7b4252d49b9d1e23555ce88e3f8e07bb3e41a298
Anish-Jha <anishj545@gmail.com>
2023-05-14 00:00:31 +0530

    file

--- models/Cart.js
+++ models/Cart.js
@@ -1,44 +1,23 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const cartItemSchema = new Schema({
+const cartItemSchema = mongoose.Schema({
   product: {
-    type: Schema.Types.ObjectId,
-    ref: 'Product'
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
   },
   quantity: {
     type: Number,
+    required: true,
     default: 1
-  }
-});
-
-const cartSchema = new Schema({
+  },
   user: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
-  },
-  items: [cartItemSchema]
+  }
 });
 
-cartSchema.methods.addItem = async function(product, quantity = 1) {
-  try {
-    const cartItem = this.items.find(item => item.product.toString() === product._id.toString());
-    if (cartItem) {
-      cartItem.quantity += quantity;
-      await this.save();
-      return cartItem;
-    } else {
-      const newCartItem = { product: product._id, quantity: quantity };
-      this.items.push(newCartItem);
-      await this.save();
-      return newCartItem;
-    }
-  } catch (error) {
-    throw error;
-  }
-};
+const CartItem = mongoose.model('CartItem', cartItemSchema);
 
-const Cart = mongoose.model('Cart', cartSchema);
-
-module.exports = Cart;
+module.exports = CartItem;
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,32 +1,30 @@
-const express = require('express');
-const Cart=require('../models/Cart')
-const Product=require('../models/Product');
-const auth=require('../middleware/auth.middleware')
-const cartRouter = express.Router();
+const User = require('../models/User');
+const Product = require('../models/Product');
 
-cartRouter.post('/addtocart/', async (req, res) => {
-    try {
-      const productId = req.body.id;
-      const user = req.user;
-      const product = await Product.findById(productId);
-      if (!product) {
-        return res.status(404).json({ message: 'Product not found' });
-      }
-      const cart = await Cart.findOne({ user: user._id, 'items.product': productId });
-      if (cart) {
-        await Cart.findOneAndUpdate(
-          { user: user._id, 'items.product': productId },
-          { $inc: { 'items.$.quantity': 1 } }
-        );
-      } else {
-        await Cart.findOneAndUpdate(
-          { user: user._id },
-          { $push: { items: { product: productId, quantity: 1 } } },
-          { upsert: true }
-        );
-      }
-    }catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
-}});
-  
+router.post('/addtocart', auth, async (req, res) => {
+  try {
+    const { productId, quantity } = req.body;
+    const user = await User.findById(req.userID);
+    const product = await Product.findById(productId);
+
+    const existingItem = user.cart.find(
+      (item) => item.product.toString() === productId
+    );
+
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      user.cart.push({
+        product: product._id,
+        quantity: quantity
+      });
+    }
+
+    await user.save();
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
