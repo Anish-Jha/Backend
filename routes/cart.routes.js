ffc05a54830d6a72ebbf924798ff79e486dcc720
Anish-Jha <anishj545@gmail.com>
2023-05-14 00:38:45 +0530

    file

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,30 +1,30 @@
 const User = require('../models/User');
+const express=require('express');
 const Product = require('../models/Product');
-
-router.post('/addtocart', auth, async (req, res) => {
+const cartRouter=express.Router();
+cartRouter.post('/addtocart', auth, async (req, res) => {
   try {
     const { productId, quantity } = req.body;
-    const user = await User.findById(req.userID);
+    const user = await User.findById({_id:req.body.userID});
     const product = await Product.findById(productId);
-
     const existingItem = user.cart.find(
       (item) => item.product.toString() === productId
     );
 
     if (existingItem) {
-      existingItem.quantity += quantity;
+      res.send({message:'Already added to cart'})
     } else {
       user.cart.push({
         product: product._id,
         quantity: quantity
       });
     }
-
     await user.save();
-
     res.json({ success: true });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: error.message });
   }
 });
+
+module.exports=cartRouter;
