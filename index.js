0b4756b97a9ffd06b7dd0aa1db159d70c2544324
Anish-Jha <anishj545@gmail.com>
2023-05-17 11:18:54 +0530

    file

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ console.log(process.env.JWT)
 app.use(express.json());
 const cors = require('cors');
 const homeRouter = require("./routes/home.routes");
+const cartRouter = require("./routes/cart.routes");
 const corsOptions ={
     origin:'*', 
     credentials:true,       
 }
 app.use(cors(corsOptions));
-app.use(userRouter,productRouter,homeRouter);
+app.use(userRouter,productRouter,homeRouter,cartRouter);
 app.get("/",(req,res)=>{
     res.send({msg:"Welcome to fashionhub app"})
 })
