9cc3f7f839903eec78469156978433e89c7b98af
Anish-Jha <anishj545@gmail.com>
2023-05-17 11:55:58 +0530

    file

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -4,39 +4,36 @@ const Cart = require('../models/Cart');
 
 // Add a product to the cart
 cartRouter.post('/addtocart', async (req, res) => {
-  try {
-    const { userId, productId, quantity, price, total } = req.body;
-
-    // Find the cart for the user
-    let carts = await Cart.findOne({ userId });
-
-    // If the cart doesn't exist, create a new one
-    if (!carts) {
-      carts = new Cart({
-        userId,
-        items: [],
-      });
+    try {
+      const { userId, productId, quantity, price, total } = req.body;
+  
+      // Find the cart for the user or create a new one
+      const cart = await Cart.findOneAndUpdate(
+        { userId },
+        { $setOnInsert: { userId, items: [] } },
+        { upsert: true, new: true }
+      );
+  
+      // Check if the product is already in the cart
+      const existingItemIndex = cart.items.findIndex((item) => item.productId === productId);
+  
+      if (existingItemIndex > -1) {
+        // Update the quantity and total of the existing item
+        cart.items[existingItemIndex].quantity += quantity;
+        cart.items[existingItemIndex].total += total;
+      } else {
+        // Add the new item to the cart
+        cart.items.push({ productId, quantity, price, total });
+      }
+  
+      await cart.save();
+      res.status(201).json({ message: 'Product added to cart successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
     }
-
-    // Check if the product is already in the cart
-    const existingItemIndex = carts.items.findIndex((item) => item.productId === productId);
-
-    if (existingItemIndex > -1) {
-      // Update the quantity and total of the existing item
-      carts.items[existingItemIndex].quantity += quantity;
-      carts.items[existingItemIndex].total += total;
-    } else {
-      // Add the new item to the cart
-      carts.items.push({ productId, quantity, price, total });
-    }
-
-    await carts.save();
-    res.status(201).json({ message: 'Product added to cart successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
+  });
+  
 
 // Get the cart for a specific user
 cartRouter.get('/:userId', async (req, res) => {
