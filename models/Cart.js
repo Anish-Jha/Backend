8c79b83b211a6b60e5837ca72c34055c357b5299
Anish-Jha <anishj545@gmail.com>
2023-05-18 00:17:37 +0530

    file

--- models/Cart.js
+++ models/Cart.js
@@ -1,25 +1,34 @@
 const mongoose = require("mongoose");
 
 const cartSchema = mongoose.Schema({
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-    required: true,
-  },
-  price: {
+  product_id: {
     type: String,
     required: true,
+    index: true, // Add index for faster lookup
+  },
+  product: {
+    image: String,
+    subhead: String,
+    name: String,
+    price: Number,
+    category: String,
+    description: String,
+    discount: String,
   },
   quantity: {
-    type: String,
+    type: Number,
     required: true,
     default: 1,
   },
   userID: {
     type: String,
     required: true,
+    index: true, // Add index for faster lookup
   },
 });
+
+// Add a unique index on product_id and userID
+cartSchema.index({ product_id: 1, userID: 1 }, { unique: true });
 
 const Cart = mongoose.model("Cart", cartSchema);
 module.exports = Cart;
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const cartRouter = express.Router();
 const Cart = require('../models/Cart');
-const auth=require('../middleware/auth.middleware')
+const auth = require('../middleware/auth.middleware');
+
 // Add a product to the cart
-cartRouter.post('/addtocart',auth, async (req, res) => {
+cartRouter.post('/addtocart', auth, async (req, res) => {
   try {
-      const carts=new Cart({
-        product_id:req.body._id,
-        price:req.body.price,
-        userID:req.body.userID
-      })
-    
-      await carts.save();
+    const { product_id, product, quantity } = req.body;
+    const userID = req.body.userID; // Assuming you have a middleware to set the user ID in req.user
+
+    // Check if the product is already in the user's cart
+    const existingItem = await Cart.findOne({ product_id, userID });
+
+    if (existingItem) {
+      return res.status(400).json({ message: 'Product already exists in the cart' });
+    }
+    // Create a new cart item
+    const cartItem = new Cart({
+      product_id,
+      product,
+      quantity,
+      userID,
+    });
+
+    await cartItem.save();
     res.status(201).json({ message: 'Product added to cart successfully' });
   } catch (error) {
     console.error(error);
@@ -20,4 +32,3 @@ cartRouter.post('/addtocart',auth, async (req, res) => {
 });
 
 module.exports = cartRouter;
-
