002e1f19d34a70e6917f16cb7a01b029886db3aa
Anish-Jha <anishj545@gmail.com>
2023-05-19 20:48:19 +0530

    file

--- middleware/admin.middleware.js
+++ middleware/admin.middleware.js
@@ -1,21 +1,15 @@
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
-const User = require('../models/User');
 
-const admin = async (req, res, next) => {
+const adminAuth = async (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
-  if (token){
+  if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT);
-      if (decoded) {
-        req.body.userID = decoded.userID;
-        req._id = decoded.userID;
-        const user = await User.findById(decoded.userID);
-        if (user && user.isAdmin) {
-          next();
-        } else {
-          res.status(400).send({ msg: "Please Login as Admin" });
-        }
+      if (decoded.isAdmin) {
+        next();
+      } else {
+        res.status(400).send({ msg: "Please Login as Admin" });
       }
     } catch (error) {
       res.status(400).send({ msg: "Please Login as Admin" });
@@ -25,4 +19,4 @@ const admin = async (req, res, next) => {
   }
 };
 
-module.exports = admin;
+module.exports = adminAuth;
